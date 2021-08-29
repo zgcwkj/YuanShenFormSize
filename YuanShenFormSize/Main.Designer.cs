de7fa7fb91b5a88c764901e5fb9740cac9c22316
zgcwkj <zgcwkj@gmail.com>
2021-08-30 00:38:25 +0800

    基础数据的读取

---
+++ YuanShenFormSize/Main.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace YuanShenFormSize
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Btn_fullScreen = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.Btn_Show1080P = new System.Windows.Forms.Button();
+            this.Btn_Show720P = new System.Windows.Forms.Button();
+            this.Btn_Ok = new System.Windows.Forms.Button();
+            this.Text_Width = new System.Windows.Forms.TextBox();
+            this.Txt_ShowType = new System.Windows.Forms.Label();
+            this.Rb_ShowForm = new System.Windows.Forms.RadioButton();
+            this.Rb_fullScreen = new System.Windows.Forms.RadioButton();
+            this.Txt_Width = new System.Windows.Forms.Label();
+            this.Txt_Height = new System.Windows.Forms.Label();
+            this.Text_Height = new System.Windows.Forms.TextBox();
+            this.Txt_Info = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_fullScreen
+            // 
+            this.Btn_fullScreen.Font = new System.Drawing.Font("宋体", 12F);
+            this.Btn_fullScreen.Location = new System.Drawing.Point(292, 19);
+            this.Btn_fullScreen.Name = "Btn_fullScreen";
+            this.Btn_fullScreen.Size = new System.Drawing.Size(156, 48);
+            this.Btn_fullScreen.TabIndex = 3;
+            this.Btn_fullScreen.Text = "全屏显示";
+            this.toolTip.SetToolTip(this.Btn_fullScreen, "全屏显示，像素最大化");
+            this.Btn_fullScreen.UseVisualStyleBackColor = true;
+            this.Btn_fullScreen.Click += new System.EventHandler(this.Btn_fullScreen_Click);
+            // 
+            // Btn_Show1080P
+            // 
+            this.Btn_Show1080P.Font = new System.Drawing.Font("宋体", 12F);
+            this.Btn_Show1080P.Location = new System.Drawing.Point(292, 87);
+            this.Btn_Show1080P.Name = "Btn_Show1080P";
+            this.Btn_Show1080P.Size = new System.Drawing.Size(83, 48);
+            this.Btn_Show1080P.TabIndex = 8;
+            this.Btn_Show1080P.Text = "1080P";
+            this.toolTip.SetToolTip(this.Btn_Show1080P, "全屏显示，像素最大化");
+            this.Btn_Show1080P.UseVisualStyleBackColor = true;
+            this.Btn_Show1080P.Click += new System.EventHandler(this.Btn_Show1080P_Click);
+            // 
+            // Btn_Show720P
+            // 
+            this.Btn_Show720P.Font = new System.Drawing.Font("宋体", 12F);
+            this.Btn_Show720P.Location = new System.Drawing.Point(381, 87);
+            this.Btn_Show720P.Name = "Btn_Show720P";
+            this.Btn_Show720P.Size = new System.Drawing.Size(69, 48);
+            this.Btn_Show720P.TabIndex = 9;
+            this.Btn_Show720P.Text = "720P";
+            this.toolTip.SetToolTip(this.Btn_Show720P, "全屏显示，像素最大化");
+            this.Btn_Show720P.UseVisualStyleBackColor = true;
+            this.Btn_Show720P.Click += new System.EventHandler(this.Btn_Show720P_Click);
+            // 
+            // Btn_Ok
+            // 
+            this.Btn_Ok.Font = new System.Drawing.Font("宋体", 12F);
+            this.Btn_Ok.Location = new System.Drawing.Point(144, 149);
+            this.Btn_Ok.Name = "Btn_Ok";
+            this.Btn_Ok.Size = new System.Drawing.Size(142, 48);
+            this.Btn_Ok.TabIndex = 10;
+            this.Btn_Ok.Text = "确定修改";
+            this.toolTip.SetToolTip(this.Btn_Ok, "全屏显示，像素最大化");
+            this.Btn_Ok.UseVisualStyleBackColor = true;
+            this.Btn_Ok.Click += new System.EventHandler(this.Btn_Ok_Click);
+            // 
+            // Text_Width
+            // 
+            this.Text_Width.Location = new System.Drawing.Point(87, 59);
+            this.Text_Width.Name = "Text_Width";
+            this.Text_Width.Size = new System.Drawing.Size(198, 25);
+            this.Text_Width.TabIndex = 5;
+            // 
+            // Txt_ShowType
+            // 
+            this.Txt_ShowType.AutoSize = true;
+            this.Txt_ShowType.Font = new System.Drawing.Font("宋体", 12F);
+            this.Txt_ShowType.Location = new System.Drawing.Point(12, 21);
+            this.Txt_ShowType.Name = "Txt_ShowType";
+            this.Txt_ShowType.Size = new System.Drawing.Size(109, 20);
+            this.Txt_ShowType.TabIndex = 0;
+            this.Txt_ShowType.Text = "显示类型：";
+            // 
+            // Rb_ShowForm
+            // 
+            this.Rb_ShowForm.AutoSize = true;
+            this.Rb_ShowForm.Font = new System.Drawing.Font("宋体", 12F);
+            this.Rb_ShowForm.Location = new System.Drawing.Point(127, 19);
+            this.Rb_ShowForm.Name = "Rb_ShowForm";
+            this.Rb_ShowForm.Size = new System.Drawing.Size(70, 24);
+            this.Rb_ShowForm.TabIndex = 1;
+            this.Rb_ShowForm.Text = "窗体";
+            this.Rb_ShowForm.UseVisualStyleBackColor = true;
+            // 
+            // Rb_fullScreen
+            // 
+            this.Rb_fullScreen.AutoSize = true;
+            this.Rb_fullScreen.Checked = true;
+            this.Rb_fullScreen.Font = new System.Drawing.Font("宋体", 12F);
+            this.Rb_fullScreen.Location = new System.Drawing.Point(212, 19);
+            this.Rb_fullScreen.Name = "Rb_fullScreen";
+            this.Rb_fullScreen.Size = new System.Drawing.Size(70, 24);
+            this.Rb_fullScreen.TabIndex = 2;
+            this.Rb_fullScreen.TabStop = true;
+            this.Rb_fullScreen.Text = "全屏";
+            this.Rb_fullScreen.UseVisualStyleBackColor = true;
+            // 
+            // Txt_Width
+            // 
+            this.Txt_Width.AutoSize = true;
+            this.Txt_Width.Font = new System.Drawing.Font("宋体", 12F);
+            this.Txt_Width.Location = new System.Drawing.Point(12, 61);
+            this.Txt_Width.Name = "Txt_Width";
+            this.Txt_Width.Size = new System.Drawing.Size(69, 20);
+            this.Txt_Width.TabIndex = 4;
+            this.Txt_Width.Text = "宽度：";
+            // 
+            // Txt_Height
+            // 
+            this.Txt_Height.AutoSize = true;
+            this.Txt_Height.Font = new System.Drawing.Font("宋体", 12F);
+            this.Txt_Height.Location = new System.Drawing.Point(12, 104);
+            this.Txt_Height.Name = "Txt_Height";
+            this.Txt_Height.Size = new System.Drawing.Size(69, 20);
+            this.Txt_Height.TabIndex = 6;
+            this.Txt_Height.Text = "高度：";
+            // 
+            // Text_Height
+            // 
+            this.Text_Height.Location = new System.Drawing.Point(88, 102);
+            this.Text_Height.Name = "Text_Height";
+            this.Text_Height.Size = new System.Drawing.Size(198, 25);
+            this.Text_Height.TabIndex = 7;
+            // 
+            // Txt_Info
+            // 
+            this.Txt_Info.AutoSize = true;
+            this.Txt_Info.ForeColor = System.Drawing.Color.Blue;
+            this.Txt_Info.Location = new System.Drawing.Point(369, 182);
+            this.Txt_Info.Name = "Txt_Info";
+            this.Txt_Info.Size = new System.Drawing.Size(79, 15);
+            this.Txt_Info.TabIndex = 11;
+            this.Txt_Info.Text = "by zgcwkj";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 209);
+            this.Controls.Add(this.Txt_Info);
+            this.Controls.Add(this.Btn_Ok);
+            this.Controls.Add(this.Btn_Show720P);
+            this.Controls.Add(this.Btn_Show1080P);
+            this.Controls.Add(this.Text_Height);
+            this.Controls.Add(this.Txt_Height);
+            this.Controls.Add(this.Txt_Width);
+            this.Controls.Add(this.Rb_fullScreen);
+            this.Controls.Add(this.Rb_ShowForm);
+            this.Controls.Add(this.Txt_ShowType);
+            this.Controls.Add(this.Text_Width);
+            this.Controls.Add(this.Btn_fullScreen);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(486, 256);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(486, 256);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "原神窗体大小";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_fullScreen;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.TextBox Text_Width;
+        private System.Windows.Forms.Label Txt_ShowType;
+        private System.Windows.Forms.RadioButton Rb_ShowForm;
+        private System.Windows.Forms.RadioButton Rb_fullScreen;
+        private System.Windows.Forms.Label Txt_Width;
+        private System.Windows.Forms.Label Txt_Height;
+        private System.Windows.Forms.TextBox Text_Height;
+        private System.Windows.Forms.Button Btn_Show1080P;
+        private System.Windows.Forms.Button Btn_Show720P;
+        private System.Windows.Forms.Button Btn_Ok;
+        private System.Windows.Forms.Label Txt_Info;
+    }
+}
+
---
+++ YuanShenFormSize/Main.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace YuanShenFormSize
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            FromBindData();
+        }
+
+        /// <summary>
+        /// 确定修改
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Ok_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 获取注册表的对象
+        /// </summary>
+        /// <returns></returns>
+        public RegistryKey GetRegistryKey()
+        {
+            string regeditPath = @"SOFTWARE\miHoYo\原神\";
+            RegistryKey key = Registry.CurrentUser;
+            RegistryKey software = key.OpenSubKey(regeditPath, true);
+            return software;
+        }
+
+        /// <summary>
+        /// 窗体与绑定注册表
+        /// </summary>
+        public void FromBindData()
+        {
+            RegistryKey software = GetRegistryKey();
+            var valueNames = software.GetValueNames();
+            var modeKey = valueNames.Where(W => W.Contains("Screenmanager Is Fullscreen mode_")).FirstOrDefault();
+            if (string.IsNullOrEmpty(modeKey))
+            {
+                MessageBox.Show("未找到显示类型", "错误");
+                this.Close();
+            }
+            Txt_ShowType.Tag = modeKey;
+            Rb_fullScreen.Checked = software.GetValue(Txt_ShowType.Tag.ToString()).ToString() == "1";
+            Rb_ShowForm.Checked = software.GetValue(Txt_ShowType.Tag.ToString()).ToString() == "0";
+            var heightKey = valueNames.Where(W => W.Contains("Screenmanager Resolution Height_")).FirstOrDefault();
+            if (string.IsNullOrEmpty(heightKey))
+            {
+                MessageBox.Show("未找到显示类型", "错误");
+                this.Close();
+            }
+            Txt_Height.Tag = heightKey;
+            Text_Height.Text = software.GetValue(Txt_Height.Tag.ToString()).ToString();
+            var widthKey = valueNames.Where(W => W.Contains("Screenmanager Resolution Width_")).FirstOrDefault();
+            if (string.IsNullOrEmpty(widthKey))
+            {
+                MessageBox.Show("未找到显示类型", "错误");
+                this.Close();
+            }
+            Txt_Width.Tag = widthKey;
+            Text_Width.Text = software.GetValue(Txt_Width.Tag.ToString()).ToString();
+        }
+
+        private void Btn_fullScreen_Click(object sender, EventArgs e)
+        {
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            Size size = new Size();
+            size.Width = GetDeviceCaps(hdc, DESKTOPHORZRES);
+            size.Height = GetDeviceCaps(hdc, DESKTOPVERTRES);
+            ReleaseDC(IntPtr.Zero, hdc);
+
+            Text_Width.Text = size.Width.ToString();
+            Text_Height.Text = size.Height.ToString();
+        }
+
+        private void Btn_Show1080P_Click(object sender, EventArgs e)
+        {
+            Text_Width.Text = "1920";
+            Text_Height.Text = "1080";
+        }
+
+        private void Btn_Show720P_Click(object sender, EventArgs e)
+        {
+            Text_Width.Text = "1280";
+            Text_Height.Text = "720";
+        }
+
+        /// <summary>
+        /// 获取 DC
+        /// </summary>
+        /// <param name="ptr"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr ptr);
+
+        /// <summary>
+        /// 获取设备 Caps
+        /// </summary>
+        /// <param name="hdc">handle to DC</param>
+        /// <param name="nIndex">index of capability</param>
+        /// <returns></returns>
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        /// <summary>
+        /// 释放 DC
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="hDc"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+        /// <summary>
+        /// DESKTOPVERTRES
+        /// </summary>
+        const int DESKTOPVERTRES = 117;
+
+        /// <summary>
+        /// DESKTOPHORZRES
+        /// </summary>
+        const int DESKTOPHORZRES = 118;
+    }
+}
